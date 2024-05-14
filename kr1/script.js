1bff2d1f7aeeb635196c6321f87a2992a8fa5011
nastyavasylyk <nastyavasylyk55@gmail.com>
2024-05-14 20:41:57 +0200

    m

--- kr1/script.js
+++ kr1/script.js
@@ -1,1 +1,72 @@
+// (1 % 2) + 1 = 1
+
+function TriangleArea(b = 5, height = 4) {
+    let square = (b * height) / 2;
+    console.log(`Square: ${b}`);
+    return square;
+  }
+  
+  TriangleArea(3, 6);
+  TriangleArea();
+  
+  function Jet(color, avgSpeed, maxAltitude, brand, pointOfDestination) {
+    this.color = color;
+    this.avgSpeed = avgSpeed;
+    this.maxAltitude = maxAltitude;
+    this.brand = brand;
+    this.pointOfDestination = pointOfDestination;
+  }
+  
+  Jet.prototype.AssignPilot = function (name, yearsOfExperience, hasChildren) {
+    this.pilot = {
+      name: name,
+      yearsOfExperience: yearsOfExperience,
+      hasChildren: hasChildren,
+    };
+  };
+  
+  let jetik = new Jet("yellow", 178, 56, "Lockheed", "Berlin");
+  jetik.AssignPilot("Anastasiia Vasylyk", "18", true);
+  console.log(jetik);
+  
+  class EquilateralTriangle {
+    constructor(equalSide) {
+      this.equalSide = equalSide;
+    }
+    get equalSide() {
+      return this._equalSide;
+    }
+    set equalSide(val) {
+      this._equalSide = val;
+    }
+  }
+  
+  class IsoscelesTriangle extends EquilateralTriangle {
+      constructor(equalSide, base) {
+          super(equalSide);
+          this.base = base;
+      }
+      square() {
+          return (this.base/4) * Math.sqrt(4*Math.pow(this.equalSide, 2) - Math.pow(this.base, 2))
+      }
+  }
+  
+  let eqTriangle = new EquilateralTriangle(4)
+  console.log(`EquilateralTriangle equal side: ${eqTriangle.equalSide}`)
+  eqTriangle.equalSide = 5
+  
+  let isTriangle = new IsoscelesTriangle(4, 5)
+  console.log(`IsoscelesTriangle square: ${isTriangle.square()}`)
+  
+  function AddGenerator(num) {
+      return function(numToAdd) {
+          return num + numToAdd
+      }
+  }
+  
+  let addTo10 = AddGenerator(10)
+  console.log(`5 + 10 = ${addTo10(5)}`)
+  
+  let addTo5 = AddGenerator(5)
+  console.log(`5 + 5 = ${addTo5(5)}`)
 
